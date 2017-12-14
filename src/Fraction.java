812382494922e53203f4b66bb398c991bb83b095
skavyas <kavya.noni@gmail.com>
2017-12-14 15:55:47 +0530

    Added test class

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,8 +1,9 @@
+import java.util.Objects;
+
 public class Fraction {
 	
 	private int numerator;
 	private int denominator;
-	private int HCF = 1;
 	
 	public Fraction(int numerator,int denominator) {
 		if(denominator == 0) {
@@ -26,11 +27,11 @@ public class Fraction {
 		return this.denominator;
 	}
 	
-	private void findHCF(int num, int den) {
+	private int findHCF(int num, int den) {
 		// TODO Auto-generated method stub
 		num = Math.abs(num);
 		den = Math.abs(den);
-		HCF = 1;
+		int HCF = 1;
 		int min;
 		if(num>den) {
 			min = den;
@@ -43,28 +44,29 @@ public class Fraction {
                 HCF = i;
                 break;
             }
-        }	
+        }
+		return HCF;
 	}
 	
 	public int findLCM(int a, int b) {
 		if(a == b) {
 			return a;
 		}
-		findHCF(a, b);
+		int HCF = findHCF(a, b);
 		return (a*b)/HCF;
 	}
 	
 	public Fraction simplify() {
 		int num = this.numerator;
 		int den = this.denominator;
-		findHCF(num,den);	
+		int HCF = findHCF(num,den);	
 		this.numerator = num/HCF;
         this.denominator = den/HCF;
 		return this;
 	}
 	
 	//Adding
-	public Fraction addFraction(Fraction f2) {
+	public Fraction add(Fraction f2) {
 		int num1 = this.numerator;
 		int den1 = this.denominator;
 		int num2 = f2.getNumerator();
@@ -77,7 +79,7 @@ public class Fraction {
 	}
 	
 	//Mulitply
-	public Fraction mulFraction(Fraction f2) {
+	public Fraction mutliply(Fraction f2) {
 		this.simplify();
 		int num1 = this.numerator;
 		int den1 = this.denominator;
@@ -117,17 +119,22 @@ public class Fraction {
 	}
 	
 	//Is equal
-	public boolean isEqual(Fraction f2) {
+	public int hashCode()
+	{
+		return Objects.hash(numerator,denominator);
+	}
+	
+	public boolean equals(Fraction f2) {
+		boolean flag = false;
 		int num1 = this.numerator;
 		int den1 = this.denominator;
 		int num2 = f2.getNumerator();
 		int den2 = f2.getDenominator();
 		int lcm = findLCM(den1, den2);
-		System.out.println("LCMM :"+lcm);
 		if(num1*(lcm/den1) == num2*(lcm/den2)) {
-			return true;
+			flag = true;
 		}
-		return false;	
+		return flag;	
 	}
 	
 	//Compare to
@@ -148,16 +155,5 @@ public class Fraction {
 		
 	}
 	
-	public void display() {
-		System.out.println(this.toString());
-	}
-	
-	public static void main(String[] args) {
-		Fraction f1  = new Fraction(-17,-17);
-		Fraction f2  = new Fraction(0,1);
-		Fraction result = f1.addFraction(f2);
-		result = f1.divideBy(f2);
-		result.display();
-	}
 	
 }
---
+++ src/Testing.java
@@ -0,0 +1,12 @@
+
+public class Testing {
+
+	public static void main(String[] args) {
+			Fraction f1  = new Fraction(17,17);
+			Fraction f2  = new Fraction(1,1);
+			Fraction result = f1.add(f2);
+			result = f1.divideBy(f2);
+			System.out.println(result);
+		}
+	
+}
