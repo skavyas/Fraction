7e93819b4af5ca2a62b7c3670f4b326a8a01bef9
skavyas <kavya.noni@gmail.com>
2017-12-14 15:56:54 +0530

    Added test class

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,71 +1,69 @@
 import java.util.Objects;
 
 public class Fraction {
-	
+
 	private int numerator;
 	private int denominator;
-	
-	public Fraction(int numerator,int denominator) {
-		if(denominator == 0) {
+
+	public Fraction(int numerator, int denominator) {
+		if (denominator == 0) {
 			throw new IllegalArgumentException("Cannot divide by zero");
 		}
-		if(denominator < 0) {
+		if (denominator < 0) {
 			this.numerator = -numerator;
 			this.denominator = -denominator;
-		}
-		else {
+		} else {
 			this.numerator = numerator;
 			this.denominator = denominator;
-		}		
+		}
 	}
-	
+
 	public int getNumerator() {
 		return this.numerator;
 	}
-	
+
 	public int getDenominator() {
 		return this.denominator;
 	}
-	
+
 	private int findHCF(int num, int den) {
 		// TODO Auto-generated method stub
 		num = Math.abs(num);
 		den = Math.abs(den);
 		int HCF = 1;
 		int min;
-		if(num>den) {
+		if (num > den) {
 			min = den;
-		}
-		else {
+		} else {
 			min = num;
 		}
 		for (int i = min; i > 1; --i) {
-            if (num%i==0&&den%i==0) {
-                HCF = i;
-                break;
-            }
-        }
+			if (num % i == 0 && den % i == 0) {
+				HCF = i;
+				break;
+			}
+		}
 		return HCF;
 	}
-	
+
 	public int findLCM(int a, int b) {
-		if(a == b) {
+		if (a == b) {
 			return a;
 		}
 		int HCF = findHCF(a, b);
-		return (a*b)/HCF;
+		return (a * b) / HCF;
 	}
-	
+
 	public Fraction simplify() {
 		int num = this.numerator;
 		int den = this.denominator;
-		int HCF = findHCF(num,den);	
-		this.numerator = num/HCF;
-        this.denominator = den/HCF;
+		int HCF = findHCF(num, den);
+		this.numerator = num / HCF;
+		this.denominator = den / HCF;
 		return this;
 	}
-	
-	//Adding
+
+	// Adding
 	public Fraction add(Fraction f2) {
 		int num1 = this.numerator;
 		int den1 = this.denominator;
@@ -73,12 +71,12 @@ public class Fraction {
 		int den2 = f2.getDenominator();
 		int lcm;
 		lcm = findLCM(den1, den2);
-		Fraction result = new Fraction((num1*(lcm/den1) + num2*(lcm/den2)),lcm);
+		Fraction result = new Fraction((num1 * (lcm / den1) + num2 * (lcm / den2)), lcm);
 		result.simplify();
-		return result;	
+		return result;
 	}
-	
-	//Mulitply
+
+	// Mulitply
 	public Fraction mutliply(Fraction f2) {
 		this.simplify();
 		int num1 = this.numerator;
@@ -86,12 +84,12 @@ public class Fraction {
 		f2.simplify();
 		int num2 = f2.getNumerator();
 		int den2 = f2.getDenominator();
-		Fraction result = new Fraction((num1*num2),(den1*den2));
+		Fraction result = new Fraction((num1 * num2), (den1 * den2));
 		result.simplify();
 		return result;
 	}
 
-	//Dividing fractions
+	// Dividing fractions
 	public Fraction divideBy(Fraction f2) {
 		this.simplify();
 		int num1 = this.numerator;
@@ -99,12 +97,12 @@ public class Fraction {
 		f2.simplify();
 		int num2 = f2.getNumerator();
 		int den2 = f2.getDenominator();
-		Fraction result = new Fraction( num1*den2,num2*den1);
+		Fraction result = new Fraction(num1 * den2, num2 * den1);
 		result.simplify();
-		return result;	
+		return result;
 	}
 
-	//Subtract fractions
+	// Subtract fractions
 	public Fraction subtractBy(Fraction f2) {
 		this.simplify();
 		int num1 = this.numerator;
@@ -113,17 +111,16 @@ public class Fraction {
 		int num2 = f2.getNumerator();
 		int den2 = f2.getDenominator();
 		int lcm = findLCM(den1, den2);
-		Fraction result = new Fraction((num1*(lcm/den1) - num2*(lcm/den2)),lcm);
+		Fraction result = new Fraction((num1 * (lcm / den1) - num2 * (lcm / den2)), lcm);
 		result.simplify();
-		return result;	
+		return result;
 	}
-	
-	//Is equal
-	public int hashCode()
-	{
-		return Objects.hash(numerator,denominator);
+
+	// Is equal
+	public int hashCode() {
+		return Objects.hash(numerator, denominator);
 	}
-	
+
 	public boolean equals(Fraction f2) {
 		boolean flag = false;
 		int num1 = this.numerator;
@@ -131,29 +128,29 @@ public class Fraction {
 		int num2 = f2.getNumerator();
 		int den2 = f2.getDenominator();
 		int lcm = findLCM(den1, den2);
-		if(num1*(lcm/den1) == num2*(lcm/den2)) {
+		if (num1 * (lcm / den1) == num2 * (lcm / den2)) {
 			flag = true;
 		}
-		return flag;	
+		return flag;
 	}
-	
-	//Compare to
+
+	// Compare to
 	public int compareTo(Fraction f2) {
 		int num1 = this.numerator;
 		int den1 = this.denominator;
 		int num2 = f2.getNumerator();
 		int den2 = f2.getDenominator();
 		int lcm = findLCM(den1, den2);
-		if(num1*(lcm/den1) <= num2*(lcm/den2)) {
+		if (num1 * (lcm / den1) <= num2 * (lcm / den2)) {
 			return -1;
 		}
-		return 1;	
+		return 1;
 	}
-	//Displaying Fraction
+
+	// Displaying Fraction
 	public String toString() {
-		return numerator+"/"+denominator;
-		
+		return numerator + "/" + denominator;
+
 	}
-	
-	
+
 }
